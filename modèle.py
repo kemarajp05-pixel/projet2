851f9be49de4b942d9c696c1aaf52be5c0837050
kemarajp05-pixel <kemarajp05@gmail.com>
2026-04-07 14:53:25 +0200

    Add files via upload

---
+++ modèle.py
@@ -0,0 +1,22 @@
+#sans fltk et sans print
+import math
+# position = (x,y)
+# v = (Vx,Vy)
+# g = (gx,gy)
+# 0 < δ ≤ 1
+# x = x + δ × vx
+# vx = vx + δ × gx
+# y = y + δ × vy
+# vy = vy + δ × gy
+
+#sert à simuler les sauts, la gravité, et le résultat des collisions.
+
+def pas(personnage, lst_blocs):
+    ...
+
+def choc(personnage, lst_blocs):
+    ...
+def collision():
+    ...
+    
+    
