68698ba715fd2978e5c1797f7f70ad8579385ddf
kemarajp05-pixel <kemarajp05@gmail.com>
2026-04-10 11:53:41 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,18 +1,20 @@
 from fltk import *
 from modelisation import *
 from calque import *
+from modèle import 
 
 personnage= {
-    "position": (450,800),
+    "position": (100,100),
     "vitesse": (0,0)
     }
-sol = 800
 
 if __name__ == "__main__":
     cree_fenetre(900,900)
+    affiche_sol(ymax)
     position=personnage["position"]
     aff_mouton(position)
     click = click()
-    print(angle(position, click, sol))
+    print(angle(position, click, ymax))
     vect_aff(position, click)
     attend_fermeture()
+    
