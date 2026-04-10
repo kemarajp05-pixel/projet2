75e1796f46e9f4afe86eadd87c98efe0814b103f
kemarajp05-pixel <kemarajp05@gmail.com>
2026-04-10 11:50:47 +0200

    Update projet.py

--- projet.py
+++ projet.py
@@ -1,14 +1,17 @@
 from fltk import *
 from modelisation import *
 from calque import *
+from modèle import *
 
 personnage= {
-    "position": (0,0),
+    "position": (100,100),
     "vitesse": (0,0)
     }
 
 if __name__ == "__main__":
     cree_fenetre(900,900)
+    affiche_sol(ymax)
     aff_mouton(personnage["position"])
     print(click())
     attend_fermeture()
+    
