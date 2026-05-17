3e5db77b172e83a4cfe9714fb19cdd0002edea45
mandrak-dev <93gabriel10@gmail.com>
2026-05-17 23:26:50 +0200

    Add files via upload

--- calque.py
+++ calque.py
@@ -1,16 +1,30 @@
 from fltk import *
 from math import *
 
-def click():
-    ev= attend_ev()
+def click(ev, menu_ouvert):
+    if ev is None:
+        return None, False, menu_ouvert
+    
     typeEv = type_ev(ev)
-    x = abscisse(ev)
-    y = ordonnee(ev)
+    x, y = abscisse(ev), ordonnee(ev)
+    
     if typeEv == "ClicGauche":
-        return (x, y), False
+        if x <= 120 and y <= 45:
+            return (x, y), False, not menu_ouvert
+        if menu_ouvert and 10 <= x <= 110 and 540 <= y <= 580:
+            return "ACCUEIL", False, menu_ouvert
+        if menu_ouvert and x<= 120 and 45 <= y <= 280:
+            return (x, y), False, True
+        return (x, y), False, menu_ouvert
+    
     if typeEv == "ClicDroit":
-        return (x, y), True
+        return (x, y), True, menu_ouvert
+    
+    return None, False, menu_ouvert
 
+
+def position_souris():
+    return (abscisse_souris(),ordonnee_souris())
 
 def vect(position, click):
     """
@@ -29,26 +43,37 @@ def norme(x, y):
 
 def vect_max(vect):
     """
-    fonction qurafraichir_ecran()i vérifie si le vecteur dépasse un certain seuil ici 20
+    fonction qurafraichir_ecran()i vérifie si le vecteur dépasse un certain seuil ici 25
     """
     x, y = vect
-    if norme(x,y)>40:
+    if norme(x,y)>25:
         return True
     return False
 
-def vect_cor(position, vect1, echelle):
+def vect_cor(position, vect1):
     """
-    corrige le vecteur à l'echelle
+    corrige le vecteur si vect_max est dépassé
     """
     x, y = position
     x2, y2 = vect1
     norme1 = norme(x2, y2)
-    x2 = echelle * (x2/norme1)
-    y2 = echelle * (y2/norme1)
+    x2 = 25 * (x2/norme1)
+    y2 = 25 * (y2/norme1)
     x1 = x + x2
     y1 = y + y2
     return (x1, y1), (x2, y2)
 
+
+def angle(position, click, sol):
+    """
+    à refaire pas complet
+    """
+    x, y = vect(position, click)
+    norme_v = norme(x,y)
+    sol1 = (click[0],sol)
+    x1, y1 = vect(sol1, click)
+    norme_s = norme(x1,y1)
+    return asin(norme_s/norme_v)
 
        
             
